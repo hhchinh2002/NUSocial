719a763b55d149f02d3dfea042a0592b1cec4600
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-05-14 20:58:11 +0530

    Create Home.js
    Missing Home.js

---
+++ pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from 'absolute_header_path'
+import LeftBar from 'absolute_leftBar_path'
+
+const Home = () => {
+  return (
+    <div>
+
+    <Header/>
+    <LeftBar/>
+    
+    </div>
+  )
+}
+
+export default Home;
