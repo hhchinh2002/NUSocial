e85ce4f7c9315130bd486d5c2fdbb9edbd1e52d7
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-05-18 13:38:56 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import PersonalChat from './pages/PersonalChat/PersonalChat';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <PersonalChat />
+    </div>
+  );
+}
+
+export default App;
