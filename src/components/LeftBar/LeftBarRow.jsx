a3df815c62e1a34f79ed7339e79631a32066af14
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-05-21 11:25:57 +0530

    explained leftBarRow props

--- src/components/LeftBar/LeftBarRow.jsx
+++ src/components/LeftBar/LeftBarRow.jsx
@@ -3,6 +3,10 @@ import {Avatar} from "@mui/material"
 import { Link } from "@mui/material"
 import './leftBarRow.css'
 
+//src is the url of the link to go to on clicking row in Left Bar
+// imgSrc is url of image shown beside text 
+// Icon is alternative to imgSrc
+// title is text that appears in row
 const LeftBarRow = ({src, imgSrc, Icon, title}) => {
   return (
     <Link href={src} style={{ textDecoration: 'none'}} >
