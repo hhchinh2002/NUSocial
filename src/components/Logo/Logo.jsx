6b499f1e651d914419d841e32c24998cf9603b10
hhchinh2002 <64810838+hhchinh2002@users.noreply.github.com>
2022-05-27 10:20:03 +0800

    Logo components

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import "./logo.css";
+import logo from "./logo.png";
+
+const Logo = () => {
+  return (
+    <div>
+        <img src={logo} alt="logo" className="logo" />
+    </div>
+  )
+}
+
+export default Logo;
