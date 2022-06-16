f8a3de53d400dc7363023c9471e6e2a9e4ba4685
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-16 21:13:22 +0530

    Create RightBar.jsx

---
+++ client/src/components/RightBar/RightBar.jsx
@@ -0,0 +1,37 @@
+import { Users, FriendSuggestion } from "../TestData/TestData";
+import './rightBar.css';
+import Online from "../Online/Online";
+import { Avatar } from "@mui/material";
+
+const RightBar = () => {
+  return (
+    <div className="rightBarContainer">
+      <div className="friendSuggestionContainer">
+        <div className="friendSuggestionTitle">
+          <span>There are <b>3</b> people that you may know</span>
+        </div>
+        <div className="friendSuggestion">
+          {FriendSuggestion.map((u) => (
+            <div className="friendSuggestionRequest">
+              <Avatar src= {u.avatar} />
+              <div className="friendSuggestName">
+                {u.username}
+              </div>
+              <button className="sendFriendRequest">Send friend request</button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="rightBarFriendListContainer">
+        <div className="friendListTitle">Friends</div>
+        <div className="rightBarFriendList">
+          {Users.map((u) => (
+            <Online key={u.id} user={u} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RightBar
