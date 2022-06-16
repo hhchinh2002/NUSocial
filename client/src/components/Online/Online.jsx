4b0407d50950c1de322d83c9c407cb0bb9feb7c1
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-16 20:34:51 +0530

    Add files via upload

---
+++ client/src/components/Online/Online.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./online.css"
+
+const Online = ({user}) => {
+  return (
+    <li className="rightBarFriend">
+        <div className="rightBarAvatarContainer">
+            <img className="rightBarAvatar" src={user.avatar} alt="" />
+            <span className="rightBarOnline"></span>
+        </div>
+        <span className="rightBarUsername">{user.userName}</span>
+    </li>
+  )
+}
+
+export default Online
