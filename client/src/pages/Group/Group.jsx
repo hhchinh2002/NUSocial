6bb6f7058cf28b1e610cc72925e89f361ea85518
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-16 20:38:37 +0530

    Add files via upload

---
+++ client/src/pages/Group/Group.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/Header/Header';
+
+ const Group = () => {
+  return (
+    <div>
+      <Header title = "Group" showHeaderCenter={true} showHeaderRight = {true} link = "/home"/>
+    </div>
+  )
+}
+
+export default Group;
