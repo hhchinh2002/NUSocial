42adbe126a59fd5ffef29740e5049dbaaf471a1b
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-17 14:24:46 +0530

    Create chatModel.js

---
+++ server/models/chatModel.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Chat = sequelize.define("Chat", {
+        sender_nusocial_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+    return Chat;
+};
+
