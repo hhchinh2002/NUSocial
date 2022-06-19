0b766e48f12ee0cb745901b0cab5189fde1f7067
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-19 09:13:21 +0530

    Update Group.jsx

--- client/src/pages/Group/Group.jsx
+++ client/src/pages/Group/Group.jsx
@@ -1,1 +1,13 @@
 
+import React from 'react'
+import Header from '../../components/Header/Header';
+
+ const Group = () => {
+  return (
+    <div>
+      <Header title = "Group" showHeaderCenter={true} showHeaderRight = {true} link = "/home"/>
+    </div>
+  )
+}
+
+export default Group;
