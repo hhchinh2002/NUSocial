9ed87c1019525327c69677baee50410407a3be40
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-19 09:27:27 +0530

    Update QuickLinks.js

--- client/src/pages/QuickLinks/QuickLinks.js
+++ client/src/pages/QuickLinks/QuickLinks.js
@@ -6,10 +6,7 @@ const QuickLinks = () => {
   return (
     <div>
       <Header title = "Quick Links" showHeaderCenter={true} showHeaderRight = {true} link = "/home" />
-      <div className = "WIP">
-      <h1>It's a WIP</h1>
-      <iframe src="https://giphy.com/embed/zw7T9RgQRm99e" width="480" height="276" frameBorder="0" class="giphy-embed" ></iframe>
-      </div>
+     Quicklinks
     
     </div>
   )
