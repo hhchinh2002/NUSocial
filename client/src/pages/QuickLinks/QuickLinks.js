05f6859d9963b14003eefc136bcc23324976d7a4
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-19 09:16:50 +0530

    Update QuickLinks.js

--- client/src/pages/QuickLinks/QuickLinks.js
+++ client/src/pages/QuickLinks/QuickLinks.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Header from '../../components/Header/Header';
+import "./QuickLinks.css"
 
 const QuickLinks = () => {
   return (
-    <div>QuickLinks</div>
+    <div>
+      <Header title = "Quick Links" showHeaderCenter={true} showHeaderRight = {true} link = "/home" />
+      <div className = "WIP">
+      <h1>It's a WIP</h1>
+      <iframe src="https://giphy.com/embed/zw7T9RgQRm99e" width="480" height="276" frameBorder="0" class="giphy-embed" ></iframe>
+      </div>
+    
+    </div>
   )
 }
 
