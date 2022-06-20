dd1a2573e53a96695ea19b81d89f089f97bea584
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-20 13:18:45 +0530

    Update RegisterStudent.js

--- client/src/pages/Register/RegisterStudent.js
+++ client/src/pages/Register/RegisterStudent.js
@@ -6,16 +6,22 @@ import { Link } from "@mui/material";
 
 
 function RegisterStudent() {
+  const [nus_email, setnus_email]= useState("");
+  const [nusnet_id, setnusnet_id] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [course, setCourse] = useState("");
+  const [course_name, setcourse_name] = useState("");
+  const [year_of_study, setyear_of_study] = useState(0);
   
 
   const addStudent = async () => {
     const data = {
+      nus_email: nus_email,
+      nusnet_id: nusnet_id,
       username: username,
       password: password,
-      course: course,
+      course_name: course_name,
+      year_of_study: year_of_study
     };
 
 const settings = {
@@ -44,6 +50,15 @@ try {
     </div>
      <div className = "RegisterStudentBody">
      <h1>Register</h1>
+     <label htmlFor="">NUS email</label>
+       <input type = "email" onChange = {(e) => {
+         setnus_email(e.target.value);
+       }} />
+   <label htmlFor="">NUSNET id</label>
+       <input type = "text" onChange = {(e) => {
+         setnusnet_id(e.target.value);
+       }} />
+    
        <label htmlFor="">Username</label>
        <input type = "text" onChange = {(e) => {
          setUsername(e.target.value);
@@ -53,17 +68,17 @@ try {
        <input type = "password" onChange = {(e) => {
          setPassword(e.target.value);
        }} />
-       <label htmlFor= "">Course</label>
+       <label htmlFor= "">Course Name</label>
        <input type = "text" onChange = {(e) => {
-         setCourse(e.target.value);
+         setcourse_name(e.target.value);
        }} />
-
+   <label htmlFor="">Year of Study</label>
+       <input type = "number" onChange = {(e) => {
+         setyear_of_study(e.target.value);
+       }} />
        <button onClick = {addStudent}>Submit</button> 
        or 
-      
        <button> <Link href = "/students-login" style={{ textDecoration: 'none' }}>Login  </Link></button>
-     
-    
      </div>
     </div>
   );
