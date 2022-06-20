7a02b206fd71c025cd93684639e69ee7402afd09
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-20 14:03:52 +0530

    Update PersonalChat.jsx

--- client/src/pages/PersonalChat/PersonalChat.jsx
+++ client/src/pages/PersonalChat/PersonalChat.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './personalChat.css'
 import SideBar from './SideBar'
 import ChatBar from './ChatBar'
 import Header from '../../components/Header/Header'
+import io from "socket.io-client"
+
 
 //SideBar contains contacts 
 // ChatBar shows chat history and section to write and send message
 // Header is by default shown on top to allow navigation to other pages
  const PersonalChat = () => {
+  const socket = io.connect("http://localhost:3001");
+  const [username, setUsername] = useState("username");
+  const [chat, setChat] = useState("chat");
+  
   return (
     <div className = "PersonalChat">
     <div className = "personalchat">
   <Header title = "Chat"  showHeaderCenter={true} showHeaderRight = {true} link = "/home"/>
     <div className = "personalchat_body">
-      <SideBar/>
-      <ChatBar  />
+      <SideBar socket = {socket}/>
+      <ChatBar socket = {socket} username = {username} chat = {chat}  />
       </div>
       </div>
     </div>
