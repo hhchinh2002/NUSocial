d67fc84430c0b3e1b1688675349b8407447838d9
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-21 22:05:54 +0530

    Update PersonalChat.jsx

--- client/src/pages/PersonalChat/PersonalChat.jsx
+++ client/src/pages/PersonalChat/PersonalChat.jsx
@@ -1,4 +1,4 @@
-
+import React, {useState} from "react"
 import './personalChat.css'
 import SideBar from './SideBar'
 import ChatBar from './ChatBar'
@@ -10,17 +10,22 @@ import { useLocation } from 'react-router-dom'
 // ChatBar shows chat history and section to write and send message
 // Header is by default shown on top to allow navigation to other pages
  const PersonalChat = ({username}) => {
+
   const socket = io.connect("http://localhost:3001");
+  const [currChat, setCurrChat] = useState('');
 
   const location = useLocation();
+  const childToParent = (childdata) => {
+    setCurrChat(childdata);
+  }
   return (
     
     <div className = "PersonalChat">
     <div className = "personalchat">
   <Header title = "Chat"  showHeaderCenter={true} showHeaderRight = {true} link = "/home"/>
     <div className = "personalchat_body">
-      <SideBar socket = {socket}/>
-      <ChatBar socket = {socket} username = {username} chat = {location.state.username}  />
+      <SideBar socket = {socket} currChat = {currChat} childToParent={childToParent} />
+      <ChatBar socket = {socket} username = {location.state.username} chat = {currChat.toString()}  />
       </div>
       </div>
     </div>
