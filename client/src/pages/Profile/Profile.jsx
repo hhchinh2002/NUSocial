09f1dfabe219da91d8dee7e846a993219011548f
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-21 17:26:00 +0530

    Update Profile.jsx

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,20 +1,24 @@
+
 import './profile.css'
 import Header from '../../components/Header/Header'
 import LeftBar from '../../components/LeftBar/LeftBar'
 import ProfileMid from '../../components/ProfileMid/ProfileMid'
+import { useLocation } from 'react-router-dom'
 
 const Profile = () => {
+  const location = useLocation();
   return (
     <div className="profile">
         <div className="profileHeader">
         <Header title = "Profile" showHeaderCenter={true} showHeaderRight={true} link = "/home"/>
         </div>
         <div className="profileBody">
+    
             <div className="profileBodyLeft">
                 <LeftBar />
             </div>
             <div className="profileBodyMid">
-                <ProfileMid />
+                <ProfileMid username = {location.state.username}  />
             </div>
         </div>
     </div>
