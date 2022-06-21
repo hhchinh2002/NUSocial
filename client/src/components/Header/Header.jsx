537c9f99763ef5556365e9dea8f3f1c03bcc9809
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-21 17:21:03 +0530

    Update Header.jsx

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './header.css';
 import { Chat, Home, Notifications, Search } from "@mui/icons-material";
 import { Avatar } from '@mui/material';
 import Logo from "../Logo/Logo";
-import { Link } from '@mui/material';
 import DoubleArrowTwoToneIcon from '@mui/icons-material/DoubleArrowTwoTone';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from "axios";
+import { Person } from '@mui/icons-material';
 
-const showingHeaderCenter = () => 
-{
-  return  (
-  <div className="headerCenter">
+const Header = ({link, title,showHeaderCenter, showHeaderRight, username}) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [selected, setSelected]= useState("");
+  const logOut = () => {
+    const data = {
+    username: location.state.username
+};
+axios.post("http://localhost:8000/api/students/logoutStudent", data).then(response => {
+   console.log(response.data);
+navigate("/")
+}).catch(
+  function (error) {
+if (error.response) {
+  // The request was made and the server responded with a status code
+  // that falls out of the range of 2xx
+  console.log(error.response.data);
+  console.log(error.response.status);
+  console.log(error.response.headers);
+}})}
+
+const handleSelect = (e) => {
+  e.target.value === "My Profile"? navigate("/profile", {state:{username: location.state.username}})
+  : e.target.value === "Log Out"? logOut(): setSelected("")}
+
+  return (
+    <div className="header">
+        <div className="headerLeft">
+          <Logo link = {link} title = {title} />
+          </div>
+        {showHeaderCenter &&   <div className="headerCenter">
   <div className="searchBar">
     <Search style={{marginLeft: "20px", fontSize: "25px"}}/>
     <input 
@@ -18,55 +49,42 @@ const showingHeaderCenter = () =>
     >
     </input>
   </div>
-  </div>
-  )
-}
-
-
-const showingHeaderRight = () => 
-{
-  return  (
-    <div className="headerRight">
+  </div>}
+        {showHeaderRight && <div className="headerRight">
     <div className="headerIcon">
       <div className="iconItem">
-      <Link href = "/home" >
-        <Home fontSize='large' htmlColor='#1f3d85'/>
-        </Link>
+        <Home fontSize='large' htmlColor='#1f3d85' onClick = {() => navigate("/home", {state:{username: location.state.username}})}/>
       </div>
       <div className="iconItem">
-      <Link href = "/newsandnots">
-        <Notifications fontSize='large' htmlColor='#1f3d85'/>
-        </Link>
+   
+        <Notifications fontSize='large' htmlColor='#1f3d85' onClick = {() => navigate("/newsandnots", {state:{username: location.state.username}})}/>
         <span className="iconBadge">4</span>
       </div>
       <div className="iconItem">
-      <Link href = "/personalChat">
-        <Chat fontSize='large' htmlColor='#1f3d85'/>
-        </Link>
-        <Link href = "/quicklinks">
-          <DoubleArrowTwoToneIcon htmlColor='#1f3d85' />
-        </Link>
+     
+        <Chat fontSize='large' htmlColor='#1f3d85' onClick = {() => navigate("/personalChat", {state:{username: location.state.username }})}/>
+          <DoubleArrowTwoToneIcon htmlColor='#1f3d85' onClick = {() => navigate("/quicklinks", {state:{username: location.state.username}})} />
+      
         <span className="iconBadge">3</span>
       </div>
     </div>
-    <Link href = "/profile">
-    <Avatar src="https://is5-ssl.mzstatic.com/image/thumb/Purple113/v4/ec/83/3a/ec833a37-1e6f-958e-9e60-4f358795405f/source/512x512bb.jpg">
+
+   
+  <Select
+    IconComponent={() => (
+      <Avatar src="https://is5-ssl.mzstatic.com/image/thumb/Purple113/v4/ec/83/3a/ec833a37-1e6f-958e-9e60-4f358795405f/source/512x512bb.jpg">
     3
     </Avatar>
-    </Link>
-  </div>
-  )
-}
+  )}
+  onChange = {handleSelect}
+  value = {selected}
+  >
+    <MenuItem value="My Profile" >My Profile</MenuItem>
+    <MenuItem value="Log Out">Log Out</MenuItem>
+  </Select>
+  
+  </div>}  
 
-
-const Header = ({link, title,showHeaderCenter, showHeaderRight}) => {
-  return (
-    <div className="header">
-        <div className="headerLeft">
-          <Logo link = {link} title = {title} />
-          </div>
-        {showHeaderCenter && showingHeaderCenter()}
-        {showHeaderRight && showingHeaderRight()}  
     </div>
   )
 }
