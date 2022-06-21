483496ac221aa844531acdaf4b8cfd512d04153b
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-21 22:30:15 +0530

    Update Group.jsx

--- client/src/pages/Group/Group.jsx
+++ client/src/pages/Group/Group.jsx
@@ -1,13 +1,28 @@
 
-import React from 'react'
-import Header from '../../components/Header/Header';
+import './group.css'
+import Header from '../../components/Header/Header'
+import LeftBar from '../../components/LeftBar/LeftBar'
+import ProfileMid from '../../components/ProfileMid/ProfileMid'
+import { useLocation } from 'react-router-dom'
 
- const Group = () => {
+const Group = () => {
+  const location = useLocation();
   return (
-    <div>
-      <Header title = "Group" showHeaderCenter={true} showHeaderRight = {true} link = "/home"/>
+    <div >
+        <div className="groupHeader">
+        <Header title = {location.state.group} showHeaderCenter={true} showHeaderRight={true} link = "/home"/>
+        </div>
+        <div className="groupBody">
+    
+            <div className="groupBodyLeft">
+                <LeftBar />
+            </div>
+            <div className="groupBodyMid">
+                <ProfileMid username = {location.state.group}  />
+            </div>
+        </div>
     </div>
   )
 }
 
-export default Group;
+export default Group
