4ad26c939700894f7060397f9db25f7268d53a35
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-21 17:26:53 +0530

    Update QuickLinks.js

--- client/src/pages/QuickLinks/QuickLinks.js
+++ client/src/pages/QuickLinks/QuickLinks.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
 import "./QuickLinks.css"
+import { useLocation } from 'react-router-dom';
 
 const QuickLinks = () => {
+  const location = useLocation();
   return (
     <div>
-      <Header title = "Quick Links" showHeaderCenter={true} showHeaderRight = {true} link = "/home" />
-     Quicklinks
+      <Header title = "Quick Links" showHeaderCenter={true} showHeaderRight = {true} link = "/home" username = {location.state.username}/>
     
     </div>
   )
