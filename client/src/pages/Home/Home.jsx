ee1f0772bb88beb70f6efa7f29c8b452a0fefba7
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-21 17:22:40 +0530

    Update Home.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,23 +4,26 @@ import News from '../../components/NewsPanel/News'
 import LeftBar from '../../components/LeftBar/LeftBar'
 import RightBar from '../../components/RightBar/RightBar'
 import './home.css'
+import { useLocation } from 'react-router-dom'
 
 
 const Home = () => {
+  const location = useLocation();
   return (
     <>
     <div className = "homeHeader">
-    <Header showHeaderCenter={true} showHeaderRight= {true}/>
+    <Header showHeaderCenter={true} showHeaderRight= {true} username = {location.state.username}/>
+   
     </div>
     <div className = "homeBody">
       <div className="homeLeftBar">
         <LeftBar />
       </div>
       <div className="homeNewsFeed">
-        <News />
+        <News  />
       </div>
       <div className="homeRightBar">
-        <RightBar />
+        <RightBar/>
       </div>
     </div>
     </>
