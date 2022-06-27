6ee942fde6d9c9d2cb54c27c58ca627ef4357589
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-27 08:33:06 +0530

    Update LaunchPage.jsx

--- client/src/pages/LaunchPage/LaunchPage.jsx
+++ client/src/pages/LaunchPage/LaunchPage.jsx
@@ -1,13 +1,51 @@
+import React, {useState} from "react"
+import { useNavigate } from "react-router-dom";
 import "./LaunchPage.css";
 import { ReactComponent as Lpimgregister } from './lpimg.svg';
 import { ReactComponent as Lpimglogin } from './lpimglogin.svg';
 import PersonIcon from '@mui/icons-material/Person';
 import HttpsIcon from '@mui/icons-material/Https';
 import EmailIcon from '@mui/icons-material/Email';
+import axios from "axios";
+import { Alert } from '@mui/material';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 export const LaunchPage = () => {
   const container = document.querySelector(".container");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [sentData, setSentData] = useState("");
+  const [open, setOpen] = useState(true);
+
+  const navigate = useNavigate();
+  const serverLogin = async () => {
+    const data = {
+      username: username,
+      password: password,
+    };
+axios.post("http://localhost:8000/api/students/findStudent", data).then(response => {
+  setSentData(response.data);
+  console.log(response.data);
+  setOpen(true)
+  console.log(username);
+})};
   
+
+const [nus_email, setnus_email]= useState("");
+const [passwordReg, setPasswordReg] = useState("");
+
+const addStudent = async () => {
+  const data = {
+    nus_email: nus_email,
+    password: passwordReg,
+  };
+axios.post("http://localhost:8000/api/students/addStudent", data).then(response => {
+setSentData(response.data);
+console.log(response.data);
+})
+};
   return (
     <div class="launchPage">  
       <div class="container">
@@ -17,17 +55,50 @@ export const LaunchPage = () => {
             <h2 class="title">Login</h2>
             <div class="inputField">
               <PersonIcon className="icon"/>
-              <input type="text" placeholder="Username" />
+              <input type="text" placeholder="Username"onChange = {(e) => {
+         setUsername(e.target.value);
+        
+       }}
+       onKeyDown = {(e) => {
+          if (e.keyCode === 13) {
+        serverLogin();
+        }
+        }} />
             </div>
             <div class="inputField">
               <HttpsIcon className="icon"/>
-              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Password" onChange = {(e) => {
+         setPassword(e.target.value);
+       }}
+        onKeyDown = {(e) => {
+          if (e.keyCode === 13) {
+        serverLogin();
+        }
+        }}/>
             </div>
-            <input type="submit" value="Login" class="btn solid" />
-            <p class="guest">Or continue as an anonymous guest</p>
+            <input type="submit" value="Login" class="btn solid"  onClick  = {serverLogin}/>
+            <p class="guest" onClick = {() => navigate("/home", {state:{username:"guest"}})}>Or continue as an anonymous guest</p>
           </form>
           <form action="#" class="registerForm">
             <h2 class="title">Login</h2>
+            {sentData === "successful login" ? navigate("/home", {state:{username:username}}) : sentData!=="" && <div><Collapse in={open}> <Alert
+      severity="warning"
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          {sentData}
+        </Alert></Collapse></div>}
             <div class="inputField">
               <PersonIcon className="icon"/>
               <input type="text" placeholder="Username" />
