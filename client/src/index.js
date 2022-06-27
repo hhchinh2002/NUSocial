95cdae23ba0c94668eaa56ed34a2ec3209d62fa0
Thing1Thing2 <93027319+Thing1Thing2@users.noreply.github.com>
2022-06-27 12:50:26 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home/Home';
-import RegisterStudent from './pages/Register/RegisterStudent';
-import LoginStudent from "./pages/Login/LoginStudent"
 import PersonalChat from "./pages/PersonalChat/PersonalChat"
 import { LaunchPage } from './pages/LaunchPage/LaunchPage';
 import QuickLinks from "./pages/QuickLinks/QuickLinks"
@@ -17,8 +15,6 @@ root.render(
   <Routes>
   <Route path = "/" element = {<LaunchPage />} />
   <Route path = "/home" element = { <Home />} />
-  <Route path = "/students-register" element = { <RegisterStudent />} />
-  <Route path = "/students-login" element = {<LoginStudent/>} />
   <Route path = "/personalchat" element = {<PersonalChat />} />
   <Route path = "/quicklinks" element = {<QuickLinks />} />
   <Route path = "/profile" element = {<Profile />} />
